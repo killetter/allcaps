0cf0a3c46291781eaf94dd6f04a5c9b12bc07056
Miguel Angel Contreras <miguel@andrepeat.xyz>
2021-09-22 21:02:35 -0500

    scropts

--- scripts/Todos los caracteres 3ra entrega.py
+++ scripts/Todos los caracteres 3ra entrega.py
@@ -1,9 +1,10 @@
 path = "../fonts/AC-Jade-Regular.otf"
-lista = ["A", "J", "j", "k", "M", "Odieresis", "R", "S", "s", "t", "u", "x", "y", "z", "at", "ampersand", "question", "exclam", "semicolon", "parenright", "quotedblright", "zero", "one", "two", "three", "four", "five", "seven", "eight", "nine"]
+lista =   [ "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "k", "L", "l", "M", "m", "N", "n", "Ntilde", "ntilde", "Odieresis", "oacute", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "w", "x", "y", "z", "at", "ampersand", "question", "exclam", "semicolon", "parenright", "quotedblright", "zero", "one", "two", "three", "four", "five", "seven", "eight", "nine", "space", "dieresis", "acute", "tilde",]
+
 
 nombre = 'Jade'
-margen_x= 39.99
-margen_y = 30
+margen_x= 44.99
+margen_y =19
 
 fs = FormattedString()
 font(path)
@@ -11,18 +12,18 @@ font(path)
 #print(x)
 fs.font(path)
 #print(x)
-fs.fontSize(135)
-fs.lineHeight(141.22)
-fs.fill(1.00,0.93,0.00)
+fs.fontSize(108)
+fs.lineHeight(111.22)
+fs.fill(1)
 
 for i, name in enumerate(lista):
     #print(name)
     fs.appendGlyph(name, "space",)
 while fs:
     #newPage('Letter')
-    newPage(1500,1500)
-    fill(0.16,0.14,0.36)
+    newPage(1280,720)
+    fill(0.0, 0.6588235294117647, 0.4196078431372549)
     rect(0,0,1500,1500)
     fs=textBox(fs, (margen_x,margen_y*.1,width()-margen_x*2,height()-margen_y*1.6), align='center')
 
-#saveImage(f'{nombre}.png')
+    saveImage(f'../Imagenes/{nombre}.png')
